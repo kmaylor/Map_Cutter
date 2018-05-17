a6bff670c0a3a16c5ad8aff60d7a32cd40ec8d95
kmaylor <kmaylor52@gmail.com>
2018-05-16 20:30:42 -0700

    added ability to cut galactic plane

--- Create_Planck_Dust_353_GHz_BK_Cuts.py
+++ Create_Planck_Dust_353_GHz_BK_Cuts.py
@@ -22,14 +22,18 @@ options,args = parser.parse_args()
 MP = MapCutter(options.dust_loc)
 sf=pysm.common.convert_units("MJysr","uK_CMB",353)
 
-delta_lon = 40.
-delta_lat = 30.
+delta_lon = 25.
+delta_lat = 25.
 step = 10.
+galactic_cut = 10.
 map_cuts = []
 
 for i,lon in enumerate(np.arange(-180,180+step-delta_lon,step)):
     for j,lat in enumerate(np.arange(-90,90+step-delta_lat,step)):
-        map_cuts.append(MP.cut_map([lon,lon+delta_lon],[lat,lat+delta_lat],2./60)*sf)
+        if np.abs(lat)<=galactic_cut: 
+            pass
+        else:
+            map_cuts.append(MP.cut_map([lon,lon+delta_lon],[lat,lat+delta_lat],2./60)*sf)
     pk.dump(map_cuts,open(options.filename,'ab'), protocol = -1)
     map_cuts = []
     print('Batch %d out of %s completed' %(i+1,(360.+step-delta_lon)/step))
